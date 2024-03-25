6eb91fb79e3242e5f8a9d63f8474f0909705559c
pchizhov <chizhov.pasha@yandex.ru>
2024-03-25 12:30:43 +0200

    Refactor helper functions

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import torch
 
+
 def sentence_features_to_device(sentence_features, target_device):
     # moves sentence feature tensors to the device
     for key in sentence_features:
@@ -8,17 +9,17 @@ def sentence_features_to_device(sentence_features, target_device):
             sentence_features[key] = sentence_features[key].to(target_device)
     return sentence_features
 
+
 def get_train_test_dataframes(dataset_path):
-    similariti_dataframe = pd.read_csv(dataset_path, index_col=0).reset_index(drop=True)
-    similariti_dataframe.dropna(inplace=True)
-    similariti_dataframe = similariti_dataframe.reset_index(drop=True)
+    similarity_dataframe = pd.read_csv(dataset_path, index_col=0).reset_index(drop=True)
+    similarity_dataframe.dropna(inplace=True)
+    similarity_dataframe = similarity_dataframe.reset_index(drop=True)
 
-    DATASET_LENGTH = len(similariti_dataframe)
-    SPLIT_POINT = DATASET_LENGTH - 1000 # 1000 samples for val set
+    DATASET_LENGTH = len(similarity_dataframe)
+    SPLIT_POINT = DATASET_LENGTH - 1000  # 1000 samples for val set
 
-
-    similariti_dataframe = similariti_dataframe.sample(frac=1, random_state=1).reset_index(drop=True) # randomize order
-    train_dataframe = similariti_dataframe.iloc[:SPLIT_POINT].reset_index(drop=True)
-    test_dataframe = similariti_dataframe.iloc[SPLIT_POINT:].reset_index(drop=True)
+    similarity_dataframe = similarity_dataframe.sample(frac=1, random_state=1).reset_index(drop=True)  # randomize order
+    train_dataframe = similarity_dataframe.iloc[:SPLIT_POINT].reset_index(drop=True)
+    test_dataframe = similarity_dataframe.iloc[SPLIT_POINT:].reset_index(drop=True)
     
     return train_dataframe, test_dataframe
